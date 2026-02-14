030a843773e54d81eacf375f2f99f372fc7117bb
Burak Tunahan Mazman <150076082+buraktnhn1@users.noreply.github.com>
2026-02-14 17:40:14 +0300

    Add Binary Search implementation (iterative) in C++

---
+++ binary_search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1; 
+}
+
+int main() {
+    int arr[] = {3, 7, 12, 20, 25, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 20;
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found." << endl;
+
+    return 0;
+}
