5ea2ff0e735e0df6302eb814117d4e2a3a1ec693
Burak Tunahan Mazman <150076082+buraktnhn1@users.noreply.github.com>
2026-02-14 17:39:15 +0300

    Add Linear Search implementation in C++

---
+++ linear_search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {5, 12, 7, 20, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 20;
+
+    int result = linearSearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found." << endl;
+
+    return 0;
+}
